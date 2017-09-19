a93342ea3fb3b425e5abcd3e916915e7dc5cf892
sayedelwany <sayedelwany03@gmail.com>
2017-09-19 02:39:32 +0200

    Organic Farm

---
+++ 8-organic farm/temp/js/plugins.js
@@ -0,0 +1,10 @@
+/*global $*/
+
+
+/*contener in slide show time change image */
+$(document).ready(function () {
+    "use strict";
+    $('.carousel').carousel({
+        interval: 5000
+    });
+});
