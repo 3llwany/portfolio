2b3fa334d6927057fba513e03f92826f019b9c2c
Sayed Elwany <31999079+3llwany@users.noreply.github.com>
2020-09-19 21:02:07 +0200

    Update main.js

--- Test/js/main.js
+++ Test/js/main.js
@@ -4,4 +4,12 @@ $(function () {
     'use strict';
      $("#myvideo")[0].setAttribute("playsinline", "");
     
+      if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+            hypeDocument.showSceneNamed('IOS Video', hypeDocument.kSceneTransitionInstant);
+        }
+            else {
+                // continue with this scene.
+            }
+
+    
 }); //End Doc ready
